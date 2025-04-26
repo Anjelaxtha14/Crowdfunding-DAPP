a1d9b536e0ed1ea6adfb371d054e05572f3ca11f
Anjela Xhrestha <anjelaxhrestha14@gmail.com>
2025-04-26 15:32:22 +0500

    Dashboard styling

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -4,27 +4,70 @@ import FundRiserForm from "../components/FundRiserForm";
 import { useSelector } from "react-redux";
 import FundRiserCard from "../components/FundRiserCard";
 import Loader from "../components/Loader";
-
+import { FaRegSmileWink } from "react-icons/fa";
 const Dashboard = () => {
-
-  const projectsList = useSelector(state=>state.projectReducer.projects)
+  const projectsList = useSelector((state) => state.projectReducer.projects);
 
   return (
-    <div className="px-2 py-4 flex flex-col lg:px-12 lg:flex-row ">
-      <div className="lg:w-7/12 my-2 lg:my-0 lg:mx-2">
-        {projectsList !== undefined?
-          projectsList.length > 0 ?
+    // <div className="px-2 py-8 flex flex-col items-center justify-center min-h-[80vh] lg:px-12">
+    //   <div className="w-full my-4 flex flex-col items-center">
+    //     {projectsList !== undefined ? (
+    //       projectsList.length > 0 ? (
+    //         projectsList.map((data, i) => (
+    //           <FundRiserCard props={data} key={i} />
+    //         ))
+    //       ) : (
+    //         <div className="flex flex-col items-center justify-center mt-10">
+    //           <FaRegSmileWink className="text-6xl text-gray-400 mb-4" />
+    //           <h1 className="text-2xl font-semibold text-gray-600 text-center font-sans">
+    //             No campaigns found currently.
+    //           </h1>
+    //           <p className="text-gray-500 text-center mt-2 max-w-md">
+    //             Start your own fundraising journey or stay tuned for some
+    //             amazing campaigns coming soon!
+    //           </p>
+    //         </div>
+    //       )
+    //     ) : (
+    //       <Loader />
+    //     )}
+    //   </div>
+
+    //   {/* Commented out the form */}
+
+    //   {/* <div className="card lg:w-5/12 h-fit my-4">
+    //     <FundRiserForm />
+    //   </div>
+    //   */}
+    // </div>
+    <div className="px-2 py-8 lg:px-12">
+      <div
+        className={`w-full my-4 flex flex-col items-center ${
+          projectsList && projectsList.length === 0
+            ? "min-h-[80vh] justify-center"
+            : ""
+        }`}
+      >
+        {projectsList !== undefined ? (
+          projectsList.length > 0 ? (
             projectsList.map((data, i) => (
-              <FundRiserCard props={data} key={i}/>
+              <FundRiserCard props={data} key={i} />
             ))
-          :
-          <h1 className="text-2xl font-bold text-gray-500 text-center font-sans">No project found !</h1>
-        :
-        <Loader/>
-      }
-      </div>
-      <div className="card lg:w-5/12 h-fit my-4">
-          <FundRiserForm/>
+          ) : (
+            <div className="flex flex-col items-center justify-center mt-10">
+              <FaRegSmileWink className="text-6xl text-gray-400 mb-4" />
+              <h1 className="text-2xl font-semibold text-gray-600 text-center font-sans">
+                No campaigns found currently.
+              </h1>
+              <p className="text-gray-500 text-center mt-2 max-w-md">
+                Start your own fundraising journey or stay tuned for some
+                amazing campaigns coming soon!
+              </p>
+            </div>
+          )
+        ) : (
+          <Loader />
+        )}
       </div>
     </div>
   );
