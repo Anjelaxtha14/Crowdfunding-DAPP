96d946d7cc94cdbd2323c79e401ca68d36f51106
Anjela Xhrestha <anjelaxhrestha14@gmail.com>
2025-04-27 01:35:31 +0500

    updated test cases

--- test/crowdfunding.test.js
+++ test/crowdfunding.test.js
@@ -1,38 +1,46 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-const etherToWei = (n) =>{
-  return ethers.utils.parseUnits(n,'ether')
-}
+const etherToWei = (n) => {
+  return ethers.utils.parseUnits(n, "ether");
+};
 
 const dateToUNIX = (date) => {
-  return Math.round(new Date(date).getTime() / 1000)
-}
+  return Math.round(new Date(date).getTime() / 1000);
+};
+
+// Update: deadline for this year-end
+const yearEndDeadline = dateToUNIX("2025-12-31");
 
 describe("Crowdfunding", () => {
-
-  var address1; 
-  var address2; 
+  var address1;
+  var address2;
   var crowdfundingContract;
 
   beforeEach(async function () {
-    [address1, address2,  ...address] = await ethers.getSigners();
+    [address1, address2, ...address] = await ethers.getSigners();
 
     const Crowdfunding = await ethers.getContractFactory("Crowdfunding");
     crowdfundingContract = await Crowdfunding.deploy();
-
-  })
+  });
 
   describe("Request for funding", async function () {
     it("Start a project", async function () {
+      const minimumContribution = etherToWei("1");
+      const deadline = yearEndDeadline;
+      const targetContribution = etherToWei("100");
+      const projectTitle = "Testing title";
+      const projectDesc = "Testing description";
 
-      const minimumContribution=etherToWei('1');
-      const deadline=dateToUNIX('2022-04-25');
-      const targetContribution=etherToWei('100');
-      const projectTitle='Testing title';
-      const projectDesc='Testing description';
-
-      const project = await crowdfundingContract.connect(address1).createProject(minimumContribution,deadline,targetContribution,projectTitle,projectDesc)
+      const project = await crowdfundingContract
+        .connect(address1)
+        .createProject(
+          minimumContribution,
+          deadline,
+          targetContribution,
+          projectTitle,
+          projectDesc
+        );
       const event = await project.wait();
 
       const projectList = await crowdfundingContract.returnAllProjects();
@@ -40,9 +48,13 @@ describe("Crowdfunding", () => {
       // Test Event
       expect(event.events.length).to.equal(1);
       expect(event.events[0].event).to.equal("ProjectStarted");
-      expect(event.events[0].args.projectContractAddress).to.equal(projectList[0]);
+      expect(event.events[0].args.projectContractAddress).to.equal(
+        projectList[0]
+      );
       expect(event.events[0].args.creator).to.equal(address1.address);
-      expect(event.events[0].args.minContribution).to.equal(minimumContribution);
+      expect(event.events[0].args.minContribution).to.equal(
+        minimumContribution
+      );
       expect(Number(event.events[0].args.projectDeadline)).to.greaterThan(0);
       expect(event.events[0].args.goalAmount).to.equal(targetContribution);
       expect(event.events[0].args.currentAmount).to.equal(0);
@@ -50,21 +62,29 @@ describe("Crowdfunding", () => {
       expect(event.events[0].args.title).to.equal(projectTitle);
       expect(event.events[0].args.desc).to.equal(projectDesc);
       expect(event.events[0].args.currentState).to.equal(0);
-
     });
 
     it("Get data", async function () {
+      const minimumContribution = etherToWei("1");
+      const deadline = yearEndDeadline;
+      const targetContribution = etherToWei("100");
+      const projectTitle = "Testing title";
+      const projectDesc = "Testing description";
 
-      const minimumContribution=etherToWei('1');
-      const deadline=dateToUNIX('2022-04-25');
-      const targetContribution=etherToWei('100');
-      const projectTitle='Testing title';
-      const projectDesc='Testing description';
-
-      await crowdfundingContract.connect(address1).createProject(minimumContribution,deadline,targetContribution,projectTitle,projectDesc)
+      await crowdfundingContract
+        .connect(address1)
+        .createProject(
+          minimumContribution,
+          deadline,
+          targetContribution,
+          projectTitle,
+          projectDesc
+        );
       const projectList = await crowdfundingContract.returnAllProjects();
-      const contribute = await crowdfundingContract.connect(address1).contribute(projectList[0],{value: etherToWei("4")});
-      
+      const contribute = await crowdfundingContract
+        .connect(address1)
+        .contribute(projectList[0], { value: etherToWei("4") });
+
       const event = await contribute.wait();
       // Test ContributionReceived event
       expect(event.events.length).to.equal(2);
@@ -72,9 +92,6 @@ describe("Crowdfunding", () => {
       expect(event.events[1].args.projectAddress).to.equal(projectList[0]);
       expect(event.events[1].args.contributedAmount).to.equal(etherToWei("4"));
       expect(event.events[1].args.contributor).to.equal(address1.address);
-
-    })
-
-  })
-
+    });
+  });
 });
